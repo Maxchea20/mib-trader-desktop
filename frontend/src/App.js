ddb507b7101515202313b6ae041553ee4f3eaff2
Maxchea20 <85250394+Maxchea20@users.noreply.github.com>
2026-09-24 22:49:15 +0800

    Remove duplicate Hunt live card from layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,6 @@ import { AiThesisPanel } from "@/components/trading/AiThesisPanel";
 import { ObservationLayer } from "@/components/trading/ObservationLayer";
 import { MultiTimeframeRegime } from "@/components/trading/MultiTimeframeRegime";
 import { KeyLevelsPanel } from "@/components/trading/KeyLevelsPanel";
-import { HuntLiveCard } from "@/components/trading/HuntLiveCard";
 import { SettingsPanel } from "@/components/trading/SettingsPanel";
 import { BacktestHuntModal } from "@/components/trading/BacktestHuntModal";
 import { PaperTradingPanel } from "@/components/trading/PaperTradingPanel";
@@ -239,20 +238,14 @@ function App() {
               <MultiTimeframeRegime htf={analysis?.htf_regime} />
               <div className="lg:col-span-2"><KeyLevelsPanel analysis={analysis} onHover={setHoveredType} /></div>
             </div>
-            <HuntLiveCard hunt={analysis?.hunt} auto={auto} />
           </section>
           <section className="col-span-12 xl:col-span-4 flex flex-col gap-3">
-            <BrainHeroPanel hunt={analysis?.hunt} weather={analysis?.weather} />
+            <BrainHeroPanel hunt={analysis?.hunt} weather={analysis?.weather} auto={auto} />
             <AiThesisPanel analysis={analysis} auto={auto} />
           </section>
         </div>
         <div className="mt-4">
-          <ObservationLayer
-            observations={analysis?.observations || []}
-            hunt={analysis?.hunt}
-            weather={analysis?.weather}
-            auto={auto}
-          />
+          <ObservationLayer observations={analysis?.observations || []} />
         </div>
         <PaperTradingPanel brain={analysis?.brain} livePrice={livePrice} timeframe={timeframe} auto={auto} onSaveAuto={saveAuto} />
       </main>
